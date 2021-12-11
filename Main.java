406a028946ca9f4809b6b4cdfe6dcccbe0192ab1
Sinay-dev <62427105+Sinay-dev@users.noreply.github.com>
2021-12-11 15:42:13 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args){
+        Menu m = new Menu();
+        System.out.println("Un client rentre dans l'auberge c'est un : "+m.getClient());
+        System.out.println("Quelle entrée voulez vous choisir ?");
+        System.out.println("[1] entrée1 ;[2] entrée2 ;[3] entrée;");
+
+        Scanner scanner = new Scanner(System.in);
+        int entree = scanner.nextInt();
+        
+        //saisie;
+
+        System.out.println("Quel plat voulez vous choisir ?");
+        System.out.println("[1] plat1 ;[2] plat2 ;[3] plat3;");
+
+        int plat = scanner.nextInt();
+
+        //saisie
+
+        System.out.println("Quel dessert voulez vous choisir ?");
+        System.out.println("[1] dessert1 ;[2] dessert2 ;[3] dessert3;");
+        int dessert = scanner.nextInt();
+
+        scanner.close();
+        // score
+        m.score();
+
+    }
+}
