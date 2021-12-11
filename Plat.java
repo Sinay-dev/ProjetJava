717682e0a9f4995b8a9d711d3a21cd877d37c771
Sinay-dev <62427105+Sinay-dev@users.noreply.github.com>
2021-12-11 14:30:16 +0100

    Create Plat.java

---
+++ Plat.java
@@ -0,0 +1,16 @@
+public abstract class Plat {
+    protected String nom;
+    protected double prix;
+    public Plat(String nom, double prix){
+        this.nom=nom;
+        this.prix=prix;
+    }
+    public double getPrix(){
+        return prix;
+    }
+    public String toString(){
+        return " nom: "+nom+" prix : "+prix;
+    }
+    public abstract String getType();
+    public abstract int getSatisfaction();
+}
