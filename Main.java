bcfc29cd69adb47eb6bf5f7b100ef7199010962e
Sinay-dev <62427105+Sinay-dev@users.noreply.github.com>
2021-12-11 15:49:51 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,12 +6,22 @@ public class Main {
         System.out.println("Un client rentre dans l'auberge c'est un : "+m.getClient());
         System.out.println("Quelle entrée voulez vous choisir ?");
         System.out.println("[1] entrée1 ;[2] entrée2 ;[3] entrée;");
-
+        
+        
+        // Saisie du choix de l'entrée.
         Scanner scanner = new Scanner(System.in);
         int entree = scanner.nextInt();
         
-        //saisie;
-
+        // On ajoute l'entrée choisi dans le menu.
+        switch(entree){
+            case 1: m.add();
+                    break;
+            case 2: m.add();
+                    break;
+            case 3: m.add();
+                    break;
+        }
+    
         System.out.println("Quel plat voulez vous choisir ?");
         System.out.println("[1] plat1 ;[2] plat2 ;[3] plat3;");
 
